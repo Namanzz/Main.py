c500b297c17958b086ca4b2dc337acbd6aaf8555
Namanzz <114794070+Namanzz@users.noreply.github.com>
2022-10-02 14:43:48 +0530

    Add files via upload

---
+++ New folder/Day_4.py
@@ -0,0 +1,27 @@
+import random
+
+# RI=random.randint(1,10)
+
+# rf=random.random()*5
+# rf=("{:.8f}".format(rf))
+# print(rf)
+# print("Welcome to compatibility test!!")
+# name1=input("Enter your name..\n")
+# name2=input("Enter thier name..\n")
+# if RI>=5:
+#     print(f"Your love score is {RI}.\nYou are compatible with your partner.")
+# else:
+#     print("You should try someone else..:(")    
+
+
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# a=random.randint(0,len(names)-1)
+# print(f"{names[a]} is going to buy the meal today!")
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[0][5])
